71e9e115ba08eb54923e7098685a3bcf262e2205
afrid2004 <mdfaisalafrid@gmail.com>
2026-08-15 23:43:20 +0600

    added production url

--- configs/db_config.php
+++ configs/db_config.php
@@ -1,17 +1,17 @@
 <?php
 
 //Remote
-// define("SERVER", "localhost");
-// define("USER", "faisalfr_fast_drop_2026");
-// define("DATABASE", "faisalfr_fast_drop_2026");
-// define("PASSWORD", "faisalfr_fast_drop_2026");
+define("SERVER", "localhost");
+define("USER", "faisalfr_todos_management");
+define("DATABASE", "faisalfr_todos_management");
+define("PASSWORD", "faisalfr_todos_management");
 
 
 //Local
-define("SERVER", "localhost");
-define("USER", "root");
-define("DATABASE", "todo_management");
-define("PASSWORD", "");
+// define("SERVER", "localhost");
+// define("USER", "root");
+// define("DATABASE", "todo_management");
+// define("PASSWORD", "");
 
 
 $db = new mysqli(SERVER, USER, PASSWORD, DATABASE);
